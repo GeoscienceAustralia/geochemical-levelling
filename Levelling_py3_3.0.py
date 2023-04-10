3afe9341339737fb58b3b739fb9bf4a9483825d2
Phil-Main <72484052+Phil-Main@users.noreply.github.com>
2023-04-11 09:56:42 +1000

    Update Levelling_py3_3.0.py

--- Levelling_py3_3.0.py
+++ Levelling_py3_3.0.py
@@ -1,4 +1,21 @@
 '''
+############################################################################
+Copyright 2022 Commonwealth of Australia (Geoscience Australia)
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+############################################################################
+
 LEVEL - can be set to 0 to calcualte the correction factors, and set to 1 in
     order to level the data.
 LIN - use 0 to use population statistics (single standard multiplicaiton) or 1
@@ -13,6 +30,7 @@ DATAONE - Legend information - X for LR (should be the original dataset).
 DATATWO - Legend information - Y for LR (should be the re-analysed dataset).
 SaveLocation - file save location.
 FileName - name of the file to be saved.
+############################################################################
 '''
 
 import math as mt
